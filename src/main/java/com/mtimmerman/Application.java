5d53768a72a7a256d419f5237dfc26b19daa4667
Maarten Timmerman <maarten@mobilevikings.com>
2015-01-02 13:55:07 +0100

    Task scheduler

--- src/main/java/com/mtimmerman/Application.java
+++ src/main/java/com/mtimmerman/Application.java
@@ -15,9 +15,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.hateoas.config.EnableEntityLinks;
 import org.springframework.hateoas.config.EnableHypermediaSupport;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
@@ -111,5 +115,34 @@ public class Application {
         );
 
         return kickAssConnector;
+    }
+
+    @Bean
+    public TaskScheduler taskScheduler() {
+        ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+        threadPoolTaskScheduler.setPoolSize(
+                25
+        );
+
+        return threadPoolTaskScheduler;
+    }
+
+    @Bean(destroyMethod = "shutdown")
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+
+        threadPoolTaskExecutor.setCorePoolSize(
+                5
+        );
+
+        threadPoolTaskExecutor.setMaxPoolSize(
+                10
+        );
+
+        threadPoolTaskExecutor.setQueueCapacity(
+                25
+        );
+
+        return threadPoolTaskExecutor;
     }
 }
