00729940e5d4454c946de07a4a44c5b8756be5da
Maarten Timmerman <maarten@mobilevikings.com>
2015-01-06 20:10:50 +0100

    disabled profile

--- src/main/java/com/mtimmerman/BackgroundTaskConfiguration.java
+++ src/main/java/com/mtimmerman/BackgroundTaskConfiguration.java
@@ -1,13 +1,11 @@
 package com.mtimmerman;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Created by maarten on 05.01.15.
  */
-@Profile("bgtasks")
 @EnableScheduling
 @Configuration
 public class BackgroundTaskConfiguration {
